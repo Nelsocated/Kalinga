7baac8b5ce43f0498f8358f2b2d46be86c3a9daf
Nelson Lago <lagonelson77@gmail.com>
2026-02-17 14:16:41 +0800

    first commit

---
+++ lib/env.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+const EnvSchema = z.object({
+  NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
+});
+
+export const env = EnvSchema.parse(process.env);
